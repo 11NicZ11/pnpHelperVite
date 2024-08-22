27b494d712ff3f08b39229725f68697f7697ad1c
11NicZ11 <u-master@gmx.de>
2024-08-22 17:29:22 +0200

    save commit

--- merus-pnp-helper/src/pages/JohnsRoomMate/Actions.jsx
+++ merus-pnp-helper/src/pages/JohnsRoomMate/Actions.jsx
@@ -2,20 +2,53 @@ function Actions() {
   return (
     <div className="Action">
       <hr></hr>
-      <h1>Action</h1>
+      <h1>Actions kampf</h1>
       <p>normal Rapier Attack: Hit +7 DMG W8+4 magic piercing</p>
       <p>
-        chromatic Rapier Attack: Hit +7 DMG W8+4 magic piercing, 2W4 Feuer,
-        Acid, Lightning, Cold
+        chromatic Rapier Attack: Hit +7 DMG W8+4 magic piercing, 2W4 Acid, Cold,
+        fire, lightning, poison
       </p>
-      <p>Mind Sliver </p>
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
-      <h1>Bonus Action</h1>
+      <p>
+        Breath weapon: Range 15 feat cone; DMG 2d10 necrotic on fail, on success
+        half dmg (increasing by 1w8 on lv 11(3W8), lv 17(4w8)); DEX save against
+        DC 15 (= 8+ con mod + PR); amount of uses depending on PR
+      </p>
+      <p>
+        Cantrip Booming Blade: hit+7; DMG W8+4 magic piercing, W8 Thunder on hit
+        and another 2w8 lightning dmg if the target moves willingly one square
+      </p>
+      <p>
+        Cantrip Green Flame Blade: hit+7; DMG W8+4 magic piercing, W8 fire on
+        hit and w8+spell mod on another target in one squre occupied
+      </p>
+      <p>Shocking grasp: hit +7; DMG 2w8</p>
+      <p>
+        R2 Blindness/Deafness (cast up to hit additional target for each slot
+        over R2)
+      </p>
+      <p>R2 Mirror Image</p>
+      <p>R3 Haste</p>
+      <p>Grapple</p>
+      <p>Shove</p>
+      <p>Disarm</p>
+      <h1>Bonus Actions kampf</h1>
 
-      <h1>Reaction</h1>
+      <p>
+        Blade Song: +1 AC, Walking speed + 10 feet, advantage on acrobatic
+        checks, +1 to consaves holding concentration, ends early if
+        incapacitated, amount of uses depending on PR
+      </p>
+      <p></p>
+      <h1>Reaction kampf</h1>
+      <p>R1 Shield</p>
+      <p>R3 Counterspell</p>
+      <p>opportunity attack</p>
+      <h1>others</h1>
+      <p>
+        Aracane recovery: oncer per day on a short rest you can recover spell
+        slots combined up to R3(half your wizard lv rounded up) not bigger then
+        R6
+      </p>
     </div>
   );
 }
--- merus-pnp-helper/src/pages/JohnsRoomMate/Zauber.jsx
+++ merus-pnp-helper/src/pages/JohnsRoomMate/Zauber.jsx
@@ -2,9 +2,46 @@ function Zauber() {
   return (
     <div className="Wizard">
       <h1>Wizard zauber prepared</h1>
+      <h1>Cantrip</h1>
+      <p>
+        Booming Blade: S,M (melee weapong worth 1sp or more); duration one
+        round; cast 1 Action; range 5 feet; Dmg W8 thunder on hit, 2W8 thunder
+        if the target moves willingly 1 square or more after beeing hit
+        (increase by one W8 at lv 11(2w8 and 3w8), lv 17(3w8 and 4w8))
+      </p>
+      <p>
+        Green-Flame Blade: S,M (melee weapong worth 1sp or more); duration: one
+        round; cast 1 Action; range 5 feet; Dmg W8 fire on hit, another creature
+        1 square next to the hit creature takes 1W8+ability mod (increase by one
+        W8 at lv 11(2w8 and 2w8), lv 17(3w8 and 3w8))
+      </p>
+      <p>
+        Light: V,M (a firefly or phosphorescent moss); Duration 1 hour; Cast
+        time 1 Action; range touch/20 feet in all directions; touch an object
+        not larger then 10 feet in any dimension, the object sheds bright light
+        in a 20-foot radius and dim light for an additional 20 feet. the light
+        can be colored as you like. you can cover the light. The spell ends if
+        you cast it again on or dismiss it as an action. target an object worn
+        by a hostile creature causes them to make an dex safe against your spell
+        casting DC
+      </p>
+      <p>
+        Shocking Grasp: V,S; duration instant; Casttime 1 action; range touch;
+        Melee spell attack aginst the target (with advantage if the target wears
+        metal armor). On Hit the target receive 2w8 lightning dmg (increase by
+        one W8 at lv 11(3w8), lv 17(4w8))
+      </p>
       <h1>Rang 1 Zauber Slots 5</h1>
+      <p>Detect Magic: Ritual</p>
+      <p>Shield</p>
       <h1>Rang 2 Zauber Slots 3</h1>
+      <p>Blindness/Deafness</p>
+      <p>Magic weapon</p>
+      <p>Mirror Image</p>
       <h1>Rang 3 Zauber Slots 2</h1>
+      <p>Blindness/deafness (rank up)</p>
+      <p>Counterspell</p>
+      <p>Haste</p>
     </div>
   );
 }
